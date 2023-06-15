a400aa64c536b5b6ed431fed96dd7f5875115f60
Nikhil Utane <nikhil.utane@gmail.com>
2023-06-15 12:45:40 +0530

    updated site URL

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'Broaden Your Mind: Dare to Think Differently',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://contrabot.vercel.app/',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
